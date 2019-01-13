daedbd29bb826a98b6ae3f414d223bc2db2249d9
WonTaek <time_to_bye@naver.com>
2019-01-14 03:43:27 +0900

    make a run code

--- Toy_Project_001/operation.py
+++ Toy_Project_001/operation.py
@@ -1,10 +1,23 @@
 from news_parser import DailyNewsCrawling
+from news_modeling import NewsModeling
 
 
-def main():
+def save_news_text():
     news = DailyNewsCrawling(2019, 1, 12)
     news.get_news_url()
     news.get_news_text()
+
+
+def save_news_modeling():
+    data = NewsMining()
+    train_data_set = data.arrange_articles('output_text.txt')
+    data.make_model(train_data_set)
+    data.draw_news_model()
+
+
+def main():
+    save_news_text()
+    save_news_modeling()
 
 
 if __name__ == '__main__':
